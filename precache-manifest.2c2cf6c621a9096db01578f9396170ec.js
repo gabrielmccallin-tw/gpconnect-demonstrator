0c495e4d22d588a2b2f646390c3eadb02c4eb3a7
gabrielmccallin-tw <gabrielmccallin-tw@users.noreply.github.com>
2020-02-16 20:41:38 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.2c2cf6c621a9096db01578f9396170ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e486dd56446a1ce08d528eec5f84685e",
+    "url": "/gpconnect-demonstrator/index.html"
+  },
+  {
+    "revision": "52ecbb73d38e02fe1f3f",
+    "url": "/gpconnect-demonstrator/static/css/2.323789c3.chunk.css"
+  },
+  {
+    "revision": "ee637ae174f7887f38fe",
+    "url": "/gpconnect-demonstrator/static/css/main.a1e2115d.chunk.css"
+  },
+  {
+    "revision": "52ecbb73d38e02fe1f3f",
+    "url": "/gpconnect-demonstrator/static/js/2.93c0ef4d.chunk.js"
+  },
+  {
+    "revision": "47f3fcf794c791004dd657bf12342682",
+    "url": "/gpconnect-demonstrator/static/js/2.93c0ef4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee637ae174f7887f38fe",
+    "url": "/gpconnect-demonstrator/static/js/main.168dcb9a.chunk.js"
+  },
+  {
+    "revision": "d8caa92be41df6924db8",
+    "url": "/gpconnect-demonstrator/static/js/runtime-main.219c01d2.js"
+  }
+]);
